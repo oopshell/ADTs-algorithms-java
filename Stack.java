e202a03b1fabe08622d22deb1c578511c66393a7
Tiantian Li <tiantiancscs@gmail.com>
2024-10-07 23:43:19 +1100

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,1 +1,12 @@
-//
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class Stack {
+    public static void main(String[] args) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println("Stack Pop: " + stack.pop());
+    }
+}
