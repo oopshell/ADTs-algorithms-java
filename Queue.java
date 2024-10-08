0aa0b9bf5bd26fe9b03964dd451e42b09ea3de3e
Tiantian Li <tiantiancscs@gmail.com>
2024-10-08 21:02:20 +1100

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -2,4 +2,12 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 public class Queue {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add("First");
+        queue.add("Second");
+        queue.add("Third");
+        System.out.println("Queue Poll: " + queue.poll());
+    }
+}
 
