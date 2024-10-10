d168cea2133f2c58b203f2caa5434fc0b2ea90f2
Tiantian Li <tiantiancscs@gmail.com>
2024-10-10 11:47:21 +1100

    Update Deque.java

--- Deque.java
+++ Deque.java
@@ -1,2 +1,12 @@
 import java.util.Deque;
 import java.util.ArrayDeque;
+
+public class DequeExample {
+    public static void main(String[] args) {
+        Deque<String> deque = new ArrayDeque<>();
+        deque.addFirst("Start");
+        deque.addLast("Middle");
+        deque.addLast("End");
+
+    }
+}
